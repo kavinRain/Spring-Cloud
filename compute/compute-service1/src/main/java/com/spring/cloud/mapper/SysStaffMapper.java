f9cd72c9e68e011af94a855d0b5035d28170364a
kwhdlp1314 <kwhdlp1314@sina.cn>
2017-03-21 11:25:42 +0800

    删除多余代码

--- compute/compute-service1/src/main/java/com/spring/cloud/mapper/SysStaffMapper.java
+++
@@ -1,9 +0,0 @@
-package com.spring.cloud.mapper;
-
-import com.spring.cloud.entity.SysStaff;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface SysStaffMapper extends Base2Mapper<SysStaff> {
-
-}
